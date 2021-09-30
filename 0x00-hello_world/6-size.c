bd108096f9c526bb60dd4285cea93c181770e707
root <mahmoudhannani3@gmail.com>
2021-09-30 07:54:43 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 /**
-* main to start
-* 
-* return 0
-*/
+ * main - Entry point
+ * Return: 0
+ */
 int main(void)
 {
 int i;
@@ -11,10 +10,11 @@ float floatt;
 char c;
 long int longint;
 long long int longlong;
+
 printf("Size of char: %zu byte(s)\n", sizeof(c));
 printf("Size of int: %zu byte(s)\n", sizeof(i));
 printf("Size of long int: %zu byte(s)\n", sizeof(longint));
 printf("Size of long long int: %zu byte(s)\n", sizeof(longlong));
 printf("Size of float: %zu byte(s)\n", sizeof(floatt));
-return(0);
+return (0);
 }
