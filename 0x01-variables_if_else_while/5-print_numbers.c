4b59838c3909892807331ff797bafdff231c0229
root <mahmoudhannani3@gmail.com>
2021-10-01 12:51:02 -0700

    print all number from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main -Entry point
+* print program that prints all single digit numbers of base 10 starting from 0
+* Return: Always 0 (Success)
+*/
+	int main(void)
+{
+	char al;
+
+	for (al = '0'; al <= '9'; al++)
+	{
+	    putchar(al);
+
+	}
+return (0);
+}
