8adfc4747e6bf161d83bda824f66d78785c0783a
root <mahmoudhannani3@gmail.com>
2021-10-05 11:55:09 -0700

    every min...

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - description for this function
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+int n;
+int m;
+for (n = 0 ; n <= 23 ; n++)
+{
+	for (m = 0 ; m <= 59 ; m++)
+		{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+		}
+}
+}
