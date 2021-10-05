655564d75d31aa583e6e2b454a42d0fba512e2dd
root <mahmoudhannani3@gmail.com>
2021-10-05 06:41:23 -0700

    print all alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - function that prints the alphabet
+*/
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+			_putchar('\n');
+			i++;
+	}
+}
