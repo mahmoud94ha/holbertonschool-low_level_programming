6dcece648a4216bbbefce765f5c458265c92d5e7
root <mahmoudhannani3@gmail.com>
2021-10-05 07:55:28 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,13 @@
 *@c: is an int
 *Return: 1 if lowercase or uppercase, 0 if otherwise
 */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 
 	if (c >= 'a' && c <= 'z')
 	return (1);
-	il else (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
+	return (1);
 	else
 	return (0);
 }
