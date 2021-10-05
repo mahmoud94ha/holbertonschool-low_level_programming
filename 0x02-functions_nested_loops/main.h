da0e10cc2d09ef7a7a23d8ea49524a8d501b0b50
root <mahmoudhannani3@gmail.com>
2021-10-05 08:50:29 -0700

    req

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include<stdlib.h>
 #include <stdio.h>
 void print_alphabet(void);
 int _putchar(char c);
@@ -7,7 +8,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
+int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
