51178f536108da7a5883b5f18fb2f8f99aab4580
root <mahmoudhannani3@gmail.com>
2021-10-05 12:23:11 -0700

    times table from 0 to 99

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * times_table - description for this function
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+for (int i = 0 ; i <= 9; i++)
+{
+for (int j = 0; j <= 9; j++)
+{
+printf("%d,  ", i * j);
+}
+printf("\n");
+return (0);
+}
+}
