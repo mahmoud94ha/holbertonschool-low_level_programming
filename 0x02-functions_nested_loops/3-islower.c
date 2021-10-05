91ea2c6562af3a9a2888829f8c352d23d3ee9e67
root <mahmoudhannani3@gmail.com>
2021-10-05 07:32:37 -0700

    function that search for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* C - is the parametre that required for the function to work
+* _islower - function that search for lowercase character.
+*/
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
