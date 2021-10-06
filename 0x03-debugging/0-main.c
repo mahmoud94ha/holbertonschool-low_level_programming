0a7435fd3d9f7ea648e44beab513a27c2bb9e64e
root <mahmoudhannani3@gmail.com>
2021-10-06 06:25:23 -0700

    main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,18 +5,12 @@
 *check if number is positive or negative
 * Return: Always 0 (Success)
 */
-void positive_or_negative(int i)
+
+int main(void)
 {
-	int n;
+int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-return (0);
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 }
