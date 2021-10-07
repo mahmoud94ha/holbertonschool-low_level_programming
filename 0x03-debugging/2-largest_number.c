b43ea9afa18b0a114e681fcd48eef8883e6ca8b5
root <mahmoudhannani3@gmail.com>
2021-10-07 04:23:30 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,19 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-		printf(largest);
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-		printf(largest);
-	}
-	else
-	{
-		largest = c;
-		printf(largest);
-	}
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > a && b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-	return (largest);
+    return (largest);
 }
+
