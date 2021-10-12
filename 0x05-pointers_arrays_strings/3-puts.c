e19632a77c75a9de59e654518a7e19c682566828
root <mahmoudhannani3@gmail.com>
2021-10-12 03:22:26 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
 *_puts -  function that prints a string, followed by a new line
-*
+*@str: is an string
 */
 void _puts(char *str)
 {
 	int i;
 
 	i = 10;
-	while(i < 20)
+	while (i < 20)
 	{
 		_putchar(*str);
 		i++;
