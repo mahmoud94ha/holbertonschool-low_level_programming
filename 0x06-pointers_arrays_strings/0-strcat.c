cc80e8b674e445bb2cb891de75a2b3bfa3c92020
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-10-14 12:40:21 +0100

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,15 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-
-	while (*(dest + i))
-		i++;
-	while (*(src))
-	{
-		*(dest + i) = *(src);
-		i++;
-		src++;
-	}
-
-	return (dest);
+int i, j;
+i = 0;
+j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+return (dest);
 }
