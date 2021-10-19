89db0f5dd265ad133c8c059d60dd6894dbb1db96
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-10-19 17:21:52 +0100

    task 4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - function
+ * @s: parametre
+ * @accept: parametre
+ * Return: char.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+i = 0;
+while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0' && s[i] != accept[j])
+j++;
+if (accept[j] == '\0')
+return (i);
+i++;
+}
+return (i);
+}
