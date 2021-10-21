eef80e8a404becc6b692e310e99fc8885c2886f6
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-10-21 21:34:36 +0100

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_print_rev_recursion -Function
+*@s: parametre
+* Return: Void
+*/
+void _print_rev_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(s[0]);
+}
+}
