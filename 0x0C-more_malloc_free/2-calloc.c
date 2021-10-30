181a6a44f1eff38d61a2efeb22fd50e38903f24a
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-10-30 12:42:38 +0100

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ **_calloc - Function
+ *@nmemb: int
+ *@size: int
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+void *ch;
+if (nmemb <= 0 || size <= 0)
+return (NULL);
+p = malloc(nmemb * size);
+ch = p;
+if (p == 0)
+{
+return (NULL);
+}
+for (i = 0; i < (nmemb * size); i++)
+p[i] = 0;
+return (ch);
+}
