eb2731c6288d84f665b928309592bf1cfcc260bd
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-17 11:40:32 +0100

    print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - fn
+ *@h: param
+ * Return: Always 0.
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t k = 0, m = 0;
+if (h == NULL)
+return (m);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+k++;
+h = h->next;
+}
+return (k);
+}
