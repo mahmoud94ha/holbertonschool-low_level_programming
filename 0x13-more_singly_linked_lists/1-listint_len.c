50682cb03beb221b519154747c801c434ff8f251
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-17 11:49:57 +0100

    list_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,18 +4,16 @@
 #include "lists.h"
 
 /**
- * print_listint - fn
+ * listint_len - fn
  *@h: param
  * Return: Always 0.
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-size_t k = 0, m = 0;
-if (h == NULL)
-return (m);
+size_t k;
+k = 0;
 while (h != NULL)
 {
-printf("%d\n", h->n);
 k++;
 h = h->next;
 }
