60d5df4d085453a6e0ce67bdf68a370c32d8a14f
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-18 20:46:31 +0100

    uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * binary_to_uint - fn
+ *@b: param
+ * Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int s, i;
+unsigned int k = 0, p = 1;
+if (b == NULL)
+return (0);
+for (s = 0; b[s];)
+s++;
+for (i = s - 1; i >= 0; i--)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+k = k + (b[i] - '0') * p;
+p = p * 2;
+}
+return (k);
+}
