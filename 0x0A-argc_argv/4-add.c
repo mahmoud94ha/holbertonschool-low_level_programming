975622e27e8ecfab0f106361fa70e958ea2c39d6
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-19 21:13:34 +0100

    task 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - function
+ *@argc: argument
+ *@argv: argument
+ *Return: int
+ */
+int main(int argc, char **argv)
+{
+int sum, i, n;
+char *p;
+sum = 0;
+if (argc > 1)
+{
+for (i = 1; argv[i]; i++)
+{
+n = strtol(argv[i], &p, 10);
+if (!*p)
+sum += n;
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+printf("%d\n", sum);
+return (0);
+}
