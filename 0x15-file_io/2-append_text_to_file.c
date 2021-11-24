c8795f85a71ab209539aa44bf0cf8177e8ccd59a
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-24 19:12:42 +0100

    text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * append_text_to_file - fn
+ * @filename: param
+ * @text_content: param
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int m, n, k;
+if (!filename)
+return (-1);
+m = open(filename, O_RDWR | O_APPEND);
+if (m == -1)
+return (-1);
+if (text_content == NULL)
+return (1);
+n = 0;
+while (text_content[n] != '\0')
+n++;
+k = write(m, text_content, n);
+if (k == -1)
+{
+return (-1);
+}
+return (1);
+}
