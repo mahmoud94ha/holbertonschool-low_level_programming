2f8a6ce10b23e3157c610bb646fb9753dcbf32f9
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-12-15 21:27:15 +0100

    4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, k;
+for (i = '0' ; i < '9' ; i++)
+{
+	for (j = '1' ; j <= '9'; j++)
+{
+	for (k = '1' ; k <= '9'; k++)
+	if (i < j && j < k)
+{
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	if (i != '7' || j != '8' || k != '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+printf("\n");
+return (0);
+}4
